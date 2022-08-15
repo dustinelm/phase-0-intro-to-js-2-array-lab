d96741ee8ed53dc59bcbc30f84d92741a595af01
Dustin <dustindavidelm@gmail.com>
2022-08-15 09:03:09 -0700

    commit tree 166bbf45c63636ca55b7813419322916445b5227

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(Ralph){
+    cats.push(Ralph)
+    return cats
+   }
+
+   function destructivelyPrependCat(Bob) {
+    cats.unshift(Bob)
+    return cats
+   }
+
+   function destructivelyRemoveLastCat() {
+   cats.pop();
+   return cats;
+   }
+
+   function destructivelyRemoveFirstCat() {
+    cats.shift();
+    return cats;
+    }
+
+
+   function appendCat(Broom) {
+    const allCats = [...cats, "Broom"];
+    return allCats
+   }
+
+   function prependCat(Arnold) {
+    const allNewCats = ["Arnold", ...cats];
+    return allNewCats
+   }
